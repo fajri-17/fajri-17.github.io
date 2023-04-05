fbee1ef31fac3a99ff20d2f5651de2cac9ab66ba
hikio-17 <fajri010217@gmail.com>
2023-04-04 23:05:13 -0700

    fix bug logic calc

--- js/index.js
+++ js/index.js
@@ -1,32 +1,42 @@
 function triangleArea() {
    let alas = document.getElementById('input_one').value;
    let tinggi = document.getElementById('input_two').value;
-   let luas = 0.5 * alas * tinggi;
+   let luas = 0.5 * Number(alas) * Number(tinggi);
    let result = document.getElementById('result');
 
+   if (alas === '' || tinggi === '') {
+      result.innerHTML = `<mark>Inputan Harus Berupa Angka!</mark>`
+   } else {
    result.innerHTML = "L = 1/2 x a x t" + "<br>" + `L = 1/2 x ${alas} X ${tinggi}` + "<br>" + `<b>L = ${luas}</b>`
+   }
 }
 
 function aroundTheTriangle() {
-   const sisiA = document.getElementById('input_one').value;
-   const sisiB = document.getElementById('input_two').value;
-   const sisiC = document.getElementById('input_three').value;
-   const keliling = Number(sisiA) + Number(sisiB) + Number(sisiC);
-   console.log(keliling)
-   const result = document.getElementById('result');
+   let sisiA = document.getElementById('input_one').value;
+   let sisiB = document.getElementById('input_two').value;
+   let sisiC = document.getElementById('input_three').value;
+   let keliling = Number(sisiA) + Number(sisiB) + Number(sisiC);
+   let result = document.getElementById('result');
 
-   result.innerHTML = "K = Sisi A + Sisi B + Sisi C" + "<br>" + `K = ${sisiA} + ${sisiB} + ${sisiC}` + "<br>" + `<b>K = ${keliling}</b>`
+   if (sisiA === '' || sisiB === '' || sisiC === '') {
+      result.innerHTML = `<mark>Inputan Harus Berupa Angka!</mark>`
+   } else {
+      result.innerHTML = "K = Sisi A + Sisi B + Sisi C" + "<br>" + `K = ${sisiA} + ${sisiB} + ${sisiC}` + "<br>" + `<b>K = ${keliling}</b>`
+   }
+   
 }
 
 function parallelogramArea() {
    let alas = document.getElementById('input_one').value;
    let tinggi = document.getElementById('input_two').value;
-   let luas = alas * tinggi;
+   let luas = Number(alas) * Number(tinggi);
    let result = document.getElementById('result');
 
-   console.log(result);
-
-   result.innerHTML = "L = a x t" + "<br>" + `L = ${alas} X ${tinggi}` + "<br>" + `<b>L = ${luas}</b>`
+   if (alas === '' || tinggi === '') {
+      result.innerHTML = `<mark>Inputan Harus Berupa Angka!</mark>`
+   } else {
+      result.innerHTML = "L = a x t" + "<br>" + `L = ${alas} X ${tinggi}` + "<br>" + `<b>L = ${luas}</b>`
+   }
 }
 
 function aroundParallelogram() {
@@ -35,7 +45,11 @@ function aroundParallelogram() {
    let keliling = 2 * (Number(sisiA) + Number(sisiB));
    let result = document.getElementById('result');
 
-   result.innerHTML = "K = 2 * (a + b)" + "<br>" + `K = ${sisiA} X ${sisiB}` + "<br>" + `<b>L = ${keliling}</b>`
+   if (sisiA === '' || sisiB === '') {
+      result.innerHTML = `<mark>Inputan Harus Berupa Angka!</mark>`
+   } else {
+      result.innerHTML = "K = 2 * (a + b)" + "<br>" + `K = 2 * (${sisiA} X ${sisiB})` + "<br>" + `<b>L = ${keliling}</b>`
+   }
 }
 
 function reset() {
@@ -51,78 +65,148 @@ function reset() {
 
 }
 
-function displayJajarGenjang() {
+function displayOptionTriangle() {
+   /** Load Page */
+   /** Option Triangle */
    document.getElementById('option').style.display = 'flex';
-   document.getElementById('change_1').innerText = 'Luas Jajar Genjang';
-   document.getElementById('change_2').innerText = 'Keliling Jajar Genjang';
-   document.getElementById('title').innerText = 'Luas Jajar Genjang';
-   document.getElementById('image').src = '/assets/img/jajar-genjang.png';
-   document.getElementById('text_1').innerText = 'Rumus Luas Jajar Genjang yaitu:';
-   document.getElementById('text_2').innerText = 'L = a x t';
-   document.getElementById('text_5').style.display = 'none';
-   document.getElementById('input_none').style.display = 'none';
-   document.getElementById('hitung_title').innerText = 'Hitung Luas Jajar Genjang';
-   document.getElementById('hitung').addEventListener('click', parallelogramArea);
+   document.getElementById('change_1').innerText = 'Luas Segitiga'
+   document.getElementById('change_2').innerText = 'Keliling Segitiga';
+
+   /** function display */
    
-   reset();
-   document.getElementById('click_change_1').addEventListener('click', displayJajarGenjang);
+   /** Clear */
+   document.getElementById('click_change_1').removeEventListener('click', displayParallelogramArea);
+   document.getElementById('click_change_2').removeEventListener('click', displayAroundParallelogram);
+
+   document.getElementById('click_change_1').addEventListener('click', displayTriangleArea);
+   document.getElementById('click_change_2').addEventListener('click', displayAroundTheTriangle);
+}
+
+function displayOptionParallelogram() {
+   /** Option Parallelogram */
+   document.getElementById('option').style.display = 'flex';
+   document.getElementById('change_1').innerText = 'Luas Jejar Genjang';
+   document.getElementById('change_2').innerText = 'Keliling Jejar Genjang';
+
+   /** Clear event listener*/
+   document.getElementById('click_change_1').removeEventListener('click', displayTriangleArea);
+   document.getElementById('click_change_2').removeEventListener('click', displayAroundTheTriangle);
+   
+   /** function display  */
+   document.getElementById('click_change_1').addEventListener('click', displayParallelogramArea);
    document.getElementById('click_change_2').addEventListener('click', displayAroundParallelogram);
 }
 
-function displayArroundTriangle() {
-   document.getElementById('title').innerText = 'Keliling Segitiga';
-   document.getElementById('image').src = '/assets/img/k-triangle.png';
-   document.getElementById('text_1').innerText = 'Rumus Keliling Segitiga yaitu:';
-   document.getElementById('text_2').innerText = 'K = S1 + S2 + S3';
-   document.getElementById('text_3').innerText = 'S1 = Sisi A';
-   document.getElementById('text_4').innerText = 'S2 = Sisi B';
-   document.getElementById('text_5').style.display = 'block';
-   document.getElementById('text_5').innerText = 'S3 = Sisi C';
-   document.getElementById('input_none').style.display = 'block';
-   document.getElementById('hitung_title').innerText = 'Hitung Keliling Segitiga';
-   document.getElementById('nilai_1').innerText = 'Sisi A'
-   document.getElementById('nilai_2').innerText = 'Sisi B'
-   document.getElementById('nilai_3').innerText = 'Sisi C'
 
-   document.getElementById('hitung').addEventListener('click', aroundTheTriangle);
+function displayTriangleArea() {
+   displayOptionTriangle();
 
-   document.getElementById('option').style.display = 'flex';
-   reset();
-}
+   /** Clear event listener */
+   document.getElementById('run_calc').removeEventListener('click', aroundTheTriangle);
+   document.getElementById('run_calc').removeEventListener('click', aroundParallelogram);
+   document.getElementById('run_calc').removeEventListener('click', parallelogramArea);
 
-function displayTriangle() {
-   document.getElementById('option').style.display = 'flex';
-   document.getElementById('change_1').innerText = 'Luas Segitiga';
-   document.getElementById('change_2').innerText = 'Keliling Segitiga';
+   document.getElementById('run_calc').addEventListener('click', triangleArea);
+   /** Content */
    document.getElementById('title').innerText = 'Luas Segitiga';
    document.getElementById('image').src = '/assets/img/segitiga.png';
    document.getElementById('text_1').innerText = 'Rumus Luas Segitiga yaitu:';
    document.getElementById('text_2').innerText = 'L = 1/2 x a x t';
-   document.getElementById('text_5').style.display = 'block';
-   document.getElementById('hitung_title').innerText = 'Hitung Luas Segitiga';
-   document.getElementById('hitung').addEventListener('click', triangleArea);
+   document.getElementById('text_3').innerText = 'L = Luas';
+   document.getElementById('text_4').innerText = 'a = Panjang Alas';
+   document.getElementById('text_5').innerText = 't = Tinggi';
+   document.getElementById('text_6').style.display = 'none';
+
+   /** Calc */
    document.getElementById('input_none').style.display = 'none';
-   
-   document.getElementById('click_change_2').addEventListener('click', displayArroundTriangle)
-   document.getElementById('click_change_1').addEventListener('click', displayTriangle)
+   document.getElementById('title_calc').innerText = 'Hitung Luas Segitiga';
+   document.getElementById('label_value_1').innerText = 'Nilai Alas';
+   document.getElementById('label_value_2').innerText = 'Nilai Tinggi';
+
+   reset();
+}
+
+function displayAroundTheTriangle() {
+   displayOptionTriangle();
+   /** Clear event listener  */
+   document.getElementById('run_calc').removeEventListener('click', triangleArea);
+   document.getElementById('run_calc').removeEventListener('click', aroundParallelogram);
+   document.getElementById('run_calc').removeEventListener('click', parallelogramArea);
+
+   document.getElementById('run_calc').addEventListener('click', aroundTheTriangle);
+
+   /** Content */
+   document.getElementById('title').innerText = 'Keliling Segitiga';
+   document.getElementById('image').src = '/assets/img/segitiga.png';
+   document.getElementById('text_1').innerText = 'Rumus Keliling Segitiga yaitu:';
+   document.getElementById('text_2').innerText = 'K = S1 + S2 + S3';
+   document.getElementById('text_3').innerText = 'K = Keliling';
+   document.getElementById('text_4').innerText = 'S1 = Panjang Sisi A';
+   document.getElementById('text_5').innerText = 'S2 = Panjang Sisi B';
+   document.getElementById('text_6').style.display = 'block';
+   document.getElementById('text_6').innerText = 'S3 = Panjang Sisi C';
+
+   /** Calc */
+   document.getElementById('title_calc').innerText = 'Hitung Keliling Segitiga';
+   document.getElementById('label_value_1').innerText = 'Panjang Sisi A';
+   document.getElementById('label_value_2').innerText = 'Panjang Sisi B';
+   document.getElementById('input_none').style.display = 'block';
+   document.getElementById('label_value_3').innerText = 'Panjang Sisi c';
+
+   reset();
+}
+
+function displayParallelogramArea() {
+   displayOptionParallelogram();
+   /** Clear event listener */
+   document.getElementById('run_calc').removeEventListener('click', aroundParallelogram);
+   document.getElementById('run_calc').removeEventListener('click', triangleArea);
+   document.getElementById('run_calc').removeEventListener('click', aroundTheTriangle);
+
+   document.getElementById('run_calc').addEventListener('click', parallelogramArea);
+   /** Content */
+   document.getElementById('title').innerText = 'Luas Jejar Genjang';
+   document.getElementById('image').src = '/assets/img/k-jajar-genjang.png';
+   document.getElementById('text_1').innerText = 'Rumus Luas Jejar Genjang yaitu:';
+   document.getElementById('text_2').innerText = 'L = a x t';
+   document.getElementById('text_3').innerText = 'L = Luas';
+   document.getElementById('text_4').innerText = 'a = Panjang Alas';
+   document.getElementById('text_5').innerText = 't = Tinggi';
+   document.getElementById('text_6').style.display = 'none';
+
+   /** Calc */
+   document.getElementById('input_none').style.display = 'none';
+   document.getElementById('title_calc').innerText = 'Hitung Luas Jejar Genjang';
+   document.getElementById('label_value_1').innerText = 'Nilai Alas';
+   document.getElementById('label_value_2').innerText = 'Nilai Tinggi';
+
    reset();
 }
 
 function displayAroundParallelogram() {
-   document.getElementById('title').innerText = 'Keliling Jajar Genjang';
+   displayOptionParallelogram();
+   /** Clear event listener */
+   document.getElementById('run_calc').removeEventListener('click', parallelogramArea);
+   document.getElementById('run_calc').removeEventListener('click', triangleArea);
+   document.getElementById('run_calc').removeEventListener('click', aroundTheTriangle);
+
+   document.getElementById('run_calc').addEventListener('click', aroundParallelogram);
+
+   /** Content */
+   document.getElementById('title').innerText = 'Keliling Jejar Genjang';
    document.getElementById('image').src = '/assets/img/k-jajar-genjang.png';
    document.getElementById('text_1').innerText = 'Rumus Keliling Jejar Genjang yaitu:';
    document.getElementById('text_2').innerText = 'K = 2 * (a + b)';
-   document.getElementById('text_3').innerText = 'a = Alas Jejar Genjang';
-   document.getElementById('text_4').innerText = 'b = Sisi Jejar Genjang Lainnya';
-   document.getElementById('text_5').style.display = 'none';
+   document.getElementById('text_3').innerText = 'K = Keliling';
+   document.getElementById('text_4').innerText = 'a = Panjang Alas sisi Jejar Genjang';
+   document.getElementById('text_5').innerText = 'b = Panjang Sisi Jejar Genjang Lainnya';
+   document.getElementById('text_6').style.display = 'none';
+
+   /** Calc */
+   document.getElementById('title_calc').innerText = 'Hitung Keliling Jejar Genjang';
+   document.getElementById('label_value_1').innerText = 'Panjang Sisi A';
+   document.getElementById('label_value_2').innerText = 'Panjang Sisi B';
    document.getElementById('input_none').style.display = 'none';
-   document.getElementById('hitung_title').innerText = 'Hitung Keliling Jejar Genjang';
-   document.getElementById('nilai_1').innerText = 'Alas Jejar Genjang';
-   document.getElementById('nilai_2').innerText = 'Sisi Jejar Genjang Lainnya';
 
-   document.getElementById('hitung').addEventListener('click', aroundParallelogram);
-
-   document.getElementById('option').style.display = 'flex';
    reset();
 }
